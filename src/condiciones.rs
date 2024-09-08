3a7baef4eb7dcf7760c4536e754d2d22d3a3b451
fran <frlopez@fi.uba.ar>
2024-09-08 15:19:52 -0300

    Arreglo errores

--- src/condiciones.rs
+++ src/condiciones.rs
@@ -1,16 +1,23 @@
 use crate::manejo_de_csv;
-use std::io::{self, BufRead,BufReader};
+use std::io::{self, BufRead, BufReader};
 use std::fs::File;
-
+use std::error::Error;
 
 #[derive(Debug)]
 pub enum Operador {
     Igual,
+    Distinto,
     Mayor,
+    MayorIgual,
     Menor,
-    And,
-    Not,
-    Or
+    MenorIgual
+}
+
+#[derive(Debug)]
+pub enum OpLogico{
+    AND,
+    OR,
+    NOT
 }
 
 
@@ -18,8 +25,25 @@ pub enum Operador {
 pub struct Condicion {
     columna: String ,
     operador: Operador,
-    valor:String
+    valor:String,
+    op_logico:OpLogico
 } 
+
+//Mal el return..
+pub fn obtener_op_logico(op:&str) -> Option<OpLogico>{
+
+    match op {
+
+        "AND" => Some(OpLogico::AND),
+        "OR" => Some(OpLogico::OR),
+        "NOT" => Some(OpLogico::NOT),
+
+        _ => Some(OpLogico::AND)
+    }
+
+}
+
+
 
 
 pub fn obtener_op(op: &str) -> Option<Operador> {
@@ -29,35 +53,38 @@ pub fn obtener_op(op: &str) -> Option<Operador> {
         "=" => Some(Operador::Igual),
         "<" => Some(Operador::Menor),
         ">" => Some(Operador::Mayor),
-        "AND" => Some(Operador::And),
-        "OR" => Some(Operador::Or),
-        "NOT" => Some(Operador::Not),
-
+        "!=" => Some(Operador::Distinto),
+        "<=" => Some(Operador::MenorIgual),
+        ">=" => Some(Operador::MayorIgual),
 
         _ => None
     }
 }
 
 
+
 pub fn procesar_condiciones(condiciones:Vec<String>) -> Vec<Condicion>{
-    
+  
     let mut condiciones_parseadas: Vec<Condicion> = Vec::new(); 
+    println!("{:?}", condiciones);
     for i in (0..condiciones.len()).step_by(4){
+        
+        if let (Some(op_logico) ,Some(op), Some(val)) = (obtener_op_logico(&condiciones[i]),obtener_op(&condiciones[i + 1]), condiciones.get(i + 2)) {
 
-        if let (Some(op), Some(val)) = (obtener_op(&condiciones[i + 1]), condiciones.get(i + 2)) {
-
-            let c = &condiciones[i];
-
+            let col: &String = &condiciones[i];
+            
+            
             let condicion = Condicion{
-                columna:c.to_string(),
+                columna:col.to_string(),
                 operador:op,
-                valor: val.to_string()
+                valor: val.to_string(),
+                op_logico:op_logico
 
             };
-
+            
             condiciones_parseadas.push(condicion);
         }
-         
+        
     };
 
     
@@ -65,54 +92,81 @@ pub fn procesar_condiciones(condiciones:Vec<String>) -> Vec<Condicion>{
 }
 
 
-pub fn comparar_condiciones(condicion:&Condicion,fila: &Vec<String>,pos:usize) -> bool{
+pub fn comparar_op(condicion:&Condicion,fila: &Vec<String>,pos:usize) -> Result<bool,String>{
 
     if let Some(valor_f) = fila.get(pos) {
 
         match condicion.operador{
 
-            Operador::Igual => valor_f == &condicion.valor,
-            Operador::Mayor => valor_f.parse::<i32>().ok() > condicion.valor.parse::<i32>().ok(),
-            Operador::Menor => valor_f.parse::<i32>().ok() < condicion.valor.parse::<i32>().ok(),
+            Operador::Igual => Ok(valor_f == &condicion.valor),
+            Operador::Mayor => Ok(valor_f.parse::<i32>().ok() > condicion.valor.parse::<i32>().ok()),
+            Operador::Menor => Ok(valor_f.parse::<i32>().ok() < condicion.valor.parse::<i32>().ok()),
+            Operador::Distinto => Ok(valor_f != &condicion.valor),
+            Operador::MayorIgual => Ok(valor_f.parse::<i32>().ok() >= condicion.valor.parse::<i32>().ok()),
+            Operador::MenorIgual => Ok(valor_f.parse::<i32>().ok() <= condicion.valor.parse::<i32>().ok()),
 
-            _ => false
+            
 
         }
 
     }
     else {
-        false
+        Err( "INVALID_COLUMN: La columna ingresada no se encuntra en el csv".to_string())
     }
 }
 
 
-pub fn comparar_con_csv(condiciones_parseadas:Vec<Condicion>, ruta_csv: String,header:Vec<String>) ->Result<Vec<Vec<String>>, io::Error>{
+pub fn comparar_op_logico(condiciones_parseadas:&Vec<Condicion>,fila: &Vec<String> ,header:&Vec<String>) -> Result <bool,String> {
+    let mut resultado = true;
+    
+    for condicion in condiciones_parseadas.iter() {
+       
+       let pos = match manejo_de_csv::obtener_posicion_header(&condicion.columna, &header) {
+            Ok(p) => p,
+            Err(e) => {
+                eprintln!("{}", e); // Error al leer el header
+                break;
+            }
+        };
+     
+
+        let segundo_resultado = match comparar_op(condicion, fila, pos) {
+                Ok(segundo_resultado) => {segundo_resultado}
+
+                Err(e) => {
+                    return Err(format!("{}", e));
+                }
+        };
+
+        match condicion.op_logico {
+            OpLogico::AND => resultado = resultado && segundo_resultado,
+            OpLogico::OR => resultado = resultado || segundo_resultado,
+            OpLogico::NOT => resultado = resultado && !segundo_resultado,
+        }
+    }
+
+   
+    Ok(resultado)
+} 
+
+
+pub fn comparar_con_csv(condiciones_parseadas:Vec<Condicion>, ruta_csv: String,header:Vec<String>) ->Result<Vec<Vec<String>>, Box<dyn Error>>{
     let archivo = File::open(&ruta_csv)?;
     let lector = BufReader::new(archivo);
     let mut matriz: Vec<Vec<String>> = Vec::new();
 
     for linea in lector.lines(){
-
+        
         let fila: Vec<String> = linea?.split(',').map(|s| s.trim().to_string()).collect();
-        let mut cumple_condiciones = true;
 
-        for condicion in &condiciones_parseadas {         
+        let cumple_condiciones = match comparar_op_logico(&condiciones_parseadas, &fila, &header){
+            Ok(segundo_resultado) => {segundo_resultado}
 
-            let pos = match manejo_de_csv::obtener_posicion_header(&condicion.columna, &header) {
-                Ok(p) => p,
-                Err(e) => {
-                    eprintln!("{} Error: No se encontro las columnas ", e); // Error de encabezado
-                    break;
-                }
-            };
 
-            if !comparar_condiciones(&condicion, &fila, pos){
-                //Si no cumple 1 lo saco ya que no lo voy a agregar (mirar como hacer con OR y NOT)
-                cumple_condiciones = false;
-                break;
-            }
+            Err(e) => return Err(Box::new(io::Error::new(io::ErrorKind::Other, e))),
+        
 
-        }
+        };
 
         if cumple_condiciones {
             matriz.push(fila);
